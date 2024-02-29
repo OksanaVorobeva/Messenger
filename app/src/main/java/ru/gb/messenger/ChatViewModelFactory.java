d90c59d21f93e0220a97a08d24825c3a3500200e
Oksana <Ksanka82@yandex.ru>
2024-02-29 18:27:31 +0300

    Messenger

---
+++ app/src/main/java/ru/gb/messenger/ChatViewModelFactory.java
@@ -0,0 +1,23 @@
+package ru.gb.messenger;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.viewmodel.CreationExtras;
+
+public class ChatViewModelFactory implements ViewModelProvider.Factory {
+
+    private String currentUserId;
+    private String otherUserId;
+
+    public ChatViewModelFactory(String currentUserId, String otherUserId) {
+        this.currentUserId = currentUserId;
+        this.otherUserId = otherUserId;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return (T) new ChatViewModel(currentUserId, otherUserId);
+    }
+}
